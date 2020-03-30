2969eb684f5c71f3eb74facd2d382eeb0ed6c2f3
Max Bondarenko <maxxbondd@gmail.com>
2020-03-31 01:00:38 +0300

    fixed state bugs, fixed audio bugs

--- src/App.js
+++ src/App.js
@@ -2,39 +2,68 @@ import React from 'react';
 import { View, SafeAreaView, Button, StyleSheet, Alert, Text, FlatList, ScrollView } from 'react-native';
 
 import io from 'socket.io-client';
-import { RTCPeerConnection, RTCView, mediaDevices, RTCSessionDescriptionType, RTCSessionDescription } from 'react-native-webrtc';
+import {
+  RTCPeerConnection,
+  RTCView,
+  mediaDevices,
+  RTCSessionDescription,
+  RTCIceCandidate
+} from 'react-native-webrtc';
 
-//const API = "http://192.168.1.102:5000";
+//const API = "http://192.168.1.100:5000";
 const API = "https://web-rtc-prototype.herokuapp.com/";
-//const RTC_CONF = { iceServers: [{ url: 'stun:stun.l.google.com:19302' }] };
-const RTC_CONF = {
-  iceServers: [{
-    url: "stun:stun.services.mozilla.com",
-    username: "somename56",
-    credential: "somecredentials6767"
-  }]
-};
 
+const RTC_CONF = { iceServers: [{ url: 'stun:stun.l.google.com:19302' }] };
 
 console.log(`API URL ${API};`);
 
 export default function App() {
   const [connected, setConnected] = React.useState(false);
+  const [connecting, setConnecting] = React.useState(false);
   const [socket, setSocket] = React.useState();
   const [users, setUsers] = React.useState([]);
 
-  const [pc, serPc] = React.useState(false);
+  const [pc, setPc] = React.useState(false);
   const [localStream, setLocalStream] = React.useState();
   const [remoteStream, setRemoteStream] = React.useState();
   const [answers, setAnswers] = React.useState({});
 
   React.useEffect(() => {
-    if (!connected && pc) {
-      //connecting to signaling server
+    if (pc) {
+      //ивент, по которому добавится удаленный стрим
+      pc.onaddstream = (e) => {
+        if (e.stream) {
+          console.log('onaddstream');
+          setRemoteStream(e.stream);
+        }
+      }
+      pc.onicecandidate = event => {
+        if (event.candidate) {
+          console.log('onicecandidate call');
+          socket.emit('add-candidate', { candidate: event.candidate });
+        }
+      };
+    }
+
+    if (!connected && pc && !connecting) {
+      //connecting to the signaling server
       try {
-        setSocket(io.connect(API));
-        setConnected(true);
-        console.log('socket: connected to server');
+        console.log('starting connecting...');
+        const s = io.connect(API);
+
+        setConnecting(true);
+
+        s.on('connect', () => {
+
+          setPc(pc);
+
+          console.log('socket: connect');
+          setSocket(s);
+          setConnected(true);
+          setConnecting(false);
+          console.log('emiting stream-ready');
+          s.emit('stream-ready');
+        })
 
       } catch (e) {
         console.log('Error: ', e);
@@ -42,6 +71,7 @@ export default function App() {
     }
 
     if (connected) {
+      console.log(socket.id);
       socket.on('add-users', (data) => {
         console.log('socket: add-users');
         setUsers([...users, ...(data.users.map((item) => ({ id: item })))]);
@@ -53,7 +83,7 @@ export default function App() {
       });
 
       //принятие предложение и запуск трансляции на своей стороне 
-      socket.on('offer-made', async (data)=>{
+      socket.on('offer-made', async (data) => {
         console.log('socket: offer-made');
         const { offer } = data;
 
@@ -67,33 +97,43 @@ export default function App() {
             answer,
             to: data.socket
           })
-        } catch(err) {
+        } catch (err) {
           console.log("error: ", err);
           Alert.alert("Cannot accept offer!");
         }
       });
 
       //если предложение принято, то запускаю процесс обмена офферами в обратную сторону
-      socket.on('answer-made', async (data)=> {
+      socket.on('answer-made', async (data) => {
         try {
           const { answer, socket } = data;
-          console.log(pc);
-          console.log(answer);
+          console.log('socket: answer-made');
           await pc.setRemoteDescription(new RTCSessionDescription(answer));
-          if(!answers[socket]) {
+          if (!answers[socket]) {
             //в обратную сторону
             createOffer(socket);
             const newAnswer = {};
             newAnswer[socket] = true;
-            setAnswers({...answer, ...newAnswer});
+            setAnswers({ ...answer, ...newAnswer });
           }
-        } catch(err) {
+        } catch (err) {
           console.log("error: ", err);
           Alert.alert("Cannot accept answer!");
         }
-       
-      });
 
+      });
+      //принятие ICE кандидата от другого пира
+      socket.on('candidate-added', async (data) => {
+        try {
+          await pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+          console.log('socket: candidate-added');
+        }
+        catch (err) {
+          Alert.alert("Error", "Error adding ICE candidate");
+          console.log('Error: ', err);
+        }
+
+      });
     }
 
     return () => {
@@ -103,10 +143,11 @@ export default function App() {
         socket.off('make-answer');
         socket.off('offer-made');
         socket.off('answer-made');
-
+        socket.off('candidate-added');
+        socket.off('connect');
       }
     }
-  }, [connected, users, socket, pc, answers]);
+  }, [connected, users, socket, pc, answers, connecting]);
 
 
   const startLocalStream = async () => {
@@ -133,19 +174,11 @@ export default function App() {
     setLocalStream(newStream);
     const peerCon = new RTCPeerConnection(RTC_CONF);
     peerCon.addStream(newStream);
-    serPc(peerCon);
 
-    //ивент, по которому добавится удаленный стрим
-    peerCon.onaddstream = (e) => {
-      if (e.stream) {
-        console.log('onaddstream');
-        setRemoteStream(e.stream);
-      }
-    }
-
+    setPc(peerCon);
   };
   const createOffer = async (id) => {
-    try{
+    try {
       const offer = await pc.createOffer();
       await pc.setLocalDescription(new RTCSessionDescription(offer));
       console.log('make-offer');
@@ -153,7 +186,7 @@ export default function App() {
         offer: offer,
         to: id
       })
-    } catch(e) {
+    } catch (e) {
       console.log(e);
       Alert.alert("Cannot send an offer.");
     }
@@ -172,48 +205,67 @@ export default function App() {
     );
   }
   return (
-    <SafeAreaView style={styles.container}>
-      {!connected && pc && <Text style={styles.text}>Connecting...</Text>}
 
-      {!localStream && <Button title="Tap to connect and start stream" onPress={startLocalStream} style={{ marginTop: 50 }} />}
-      {connected &&
-        <View styles={styles.usersBlock}>
-          <Text style={{ ...styles.text, marginTop: 20 }}>Users</Text>
-          <FlatList
-            style={styles.users}
-            data={users}
-            renderItem={renderUser}
-            keyExtractor={(item) => item.id}
-          />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        {connecting && <Text style={styles.text}>Connecting...</Text>}
+        {connected && socket && <Text style={styles.text}>You: {<Text style={styles.smallText}>{socket.id}</Text>}</Text>}
 
+        {!localStream && <Button title="Tap to connect and start stream" onPress={startLocalStream} style={{ marginTop: 50 }} />}
+        {connected &&
+          <View styles={styles.usersBlock}>
+            <Text style={{ ...styles.text, marginTop: 20 }}>Users</Text>
+            <FlatList
+              style={styles.users}
+              data={users}
+              renderItem={renderUser}
+              keyExtractor={(item) => item.id}
+            />
+
+          </View>
+
+        }
+      </View>
+
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <View style={styles.rtcview}>
+          {localStream && <RTCView style={styles.rtc} streamURL={localStream.toURL()} />}
         </View>
+        <View style={styles.rtcview}>
+          {remoteStream && <RTCView style={styles.rtc} streamURL={remoteStream.toURL()} />}
+        </View>
+      </View>
 
-      }
-      <View style={styles.rtcview}>
-        {localStream && <RTCView style={styles.rtc} streamURL={localStream.toURL()} />}
-      </View>
-      <View style={styles.rtcview}>
-        {remoteStream && <RTCView style={styles.rtc} streamURL={remoteStream.toURL()} />}
-      </View>
-    </SafeAreaView>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#313131',
-    //justifyContent: 'flex-start',
+    justifyContent: 'flex-start',
     //flexDirection: 'column',
     //alignItems: 'center',
+
+    //alignItems: 'center',
     height: '100%',
+
   },
   usersBlock: {
-    justifyContent: 'flex-start',
+    flex: 1,
+    width: '%80',
+    justifyContent: 'center',
     alignItems: 'center',
     padding: 15
   },
   text: {
     fontSize: 30,
+    color: 'white'
+  },
+  smallText: {
+    fontSize: 15,
     color: 'white'
   },
   rtcview: {
@@ -222,6 +274,7 @@ const styles = StyleSheet.create({
     height: '40%',
     width: '80%',
     backgroundColor: 'black',
+    margin: 20
   },
   rtc: {
     width: '80%',
@@ -236,6 +289,10 @@ const styles = StyleSheet.create({
     paddingVertical: 10
   },
   userButton: {
-    marginTop: 10
+    marginTop: 10,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15
   }
 });
